5f711ac0256594ceb2b5df3cedef10c4fb974527
Andrew Davisson <akdavisson4@gmail.com>
2020-06-30 21:02:50 -0600

    added buttons and title and styling

--- src/views/MenuScreen.js
+++ src/views/MenuScreen.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Button } from 'react-native-paper'
 
 const MenuScreen = () => {
   return (
-    <View>
-      <Text>Menu</Text>
+    <View style={styles.menuView}>
+      <Text style={styles.header}>Let's Get Started!</Text>
+      <Button mode='contained' color='#f66783' style={{...styles.button, marginTop: '50%'}} labelStyle={{ color: '#522d80' }}>
+        Choose Story
+      </Button>
+      <Button mode='contained' color='#f66783' style={styles.button} labelStyle={{ color: '#522d80' }}>
+        Random Story
+      </Button>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  
+  menuView: {
+    flex: 1,
+    backgroundColor: '#522d80',
+    alignItems: 'center',
+  },
+  header: {
+    marginTop: '25%',
+    fontSize: 36,
+    color: '#f66783',
+  },
+  button: {
+    width: '50%',
+    margin: 25,
+  },
 })
- 
-export default MenuScreen;
+
+export default MenuScreen
