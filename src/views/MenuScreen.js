79b0c125c05da97add8651a60132e3bdc96a91d3
Andrew Davisson <akdavisson4@gmail.com>
2020-06-30 22:16:12 -0600

    link button to story select screen

--- src/views/MenuScreen.js
+++ src/views/MenuScreen.js
@@ -2,14 +2,25 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Button } from 'react-native-paper'
 
-const MenuScreen = () => {
+const MenuScreen = ({ navigation }) => {
   return (
     <View style={styles.menuView}>
       <Text style={styles.header}>Let's Get Started!</Text>
-      <Button mode='contained' color='#f66783' style={{...styles.button, marginTop: '50%'}} labelStyle={{ color: '#522d80' }}>
+      <Button
+        mode='contained'
+        color='#f66783'
+        style={{ ...styles.button, marginTop: '50%' }}
+        labelStyle={{ color: '#522d80' }}
+        onPress={() => navigation.navigate('SelectStory')}
+      >
         Choose Story
       </Button>
-      <Button mode='contained' color='#f66783' style={styles.button} labelStyle={{ color: '#522d80' }}>
+      <Button
+        mode='contained'
+        color='#f66783'
+        style={styles.button}
+        labelStyle={{ color: '#522d80' }}
+      >
         Random Story
       </Button>
     </View>
