1050efdf9c96bfec83dfe27fa327706abca14d6e
Andrew Davisson <akdavisson4@gmail.com>
2020-06-30 23:04:53 -0600

    need to choos different selector

--- src/views/StorySelectScreen.js
+++ src/views/StorySelectScreen.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import RNPickerSelect from 'react-native-picker-select'
 
 const StorySelectScreen = () => {
+  const data = [
+    { label: 'apple', value: 'apple' },
+    { label: 'orange', value: 'orange' },
+    { label: 'banana', value: 'banana' },
+  ]
+
   return (
     <View style={styles.storySelectView}>
-      <Text>Select a Story</Text>
+      <Text style={styles.header}>Select a Story</Text>
+      <View style={styles.dropdownContainer}>
+        <RNPickerSelect
+          style={styles.dropdown}
+          items={data}
+          onValueChange={(value) => console.log(value)}
+        />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +29,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#522d80',
     alignItems: 'center',
   },
+  header: {
+    marginTop: '25%',
+    fontSize: 36,
+    color: '#f66783',
+  },
+  dropdownContainer: {
+    marginTop: '35%',
+    width: '65%',
+  },
+  dropdown: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'orange',
+  }
 })
- 
-export default StorySelectScreen;
+
+export default StorySelectScreen
