5a75ee2e5d934cdc883728af88d906183a40a52f
Andrew Davisson <akdavisson4@gmail.com>
2020-06-30 22:27:27 -0600

    styling

--- src/views/StorySelectScreen.js
+++ src/views/StorySelectScreen.js
@@ -3,14 +3,18 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const StorySelectScreen = () => {
   return (
-    <View>
+    <View style={styles.storySelectView}>
       <Text>Select a Story</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  storySelectView: {
+    flex: 1,
+    backgroundColor: '#522d80',
+    alignItems: 'center',
+  },
 })
  
 export default StorySelectScreen;
