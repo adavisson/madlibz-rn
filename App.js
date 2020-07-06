24450dcdab7210dfe193886dbcd89a5573926a76
Andrew Davisson <akdavisson4@gmail.com>
2020-07-06 10:21:41 -0600

    added solution screen

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { Provider as PaperProvider } from 'react-native-paper'
 import HomeScreen from './src/views/HomeScreen'
 import MenuScreen from './src/views/MenuScreen'
 import FormScreen from './src/views/FormScreen'
+import SolutionScreen from './src/views/SolutionScreen'
 
 const navigator = createStackNavigator(
   {
     Home: {
       screen: HomeScreen,
       navigationOptions: {
-        headerShown: false
+        headerShown: false,
       },
     },
     Menu: {
       screen: MenuScreen,
     },
     Form: {
-      screen: FormScreen
-    }
+      screen: FormScreen,
+    },
+    Solution: {
+      screen: SolutionScreen,
+    },
   },
   {
     initialRouteName: 'Home',
@@ -28,15 +32,15 @@ const navigator = createStackNavigator(
       headerStyle: {
         backgroundColor: '#f66783',
       },
-      headerTintColor: '#522d80'
-    }
-  },
+      headerTintColor: '#522d80',
+    },
+  }
 )
 
 const Navigator = createAppContainer(navigator)
 
 export default function App() {
-  return(
+  return (
     <PaperProvider>
       <Navigator />
     </PaperProvider>
---
+++ src/views/SolutionScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const SolutionScreen = () => {
+  return (  
+    <View>
+      <Text>Solution</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default SolutionScreen;
