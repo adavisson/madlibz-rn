cb0d24161786d217bfe998902bb0c6dee666b1cc
Andrew Davisson <akdavisson4@gmail.com>
2020-07-13 11:31:00 -0600

    templated solutiondialog

---
+++ src/components/SolutionDialog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const SolutionDialog = () => {
+  return (
+    <View>
+      <Text>Solution</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default SolutionDialog;
